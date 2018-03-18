e93fe53ed6fb2368f35d660234b462d2990e6ef0
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-18 12:31:01 -0700

    Created and added the SideDrawer component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
 const Layout = props => (
   <Aux>
     <Toolbar/>
+    <SideDrawer/>
     <main className={classes.Content}>
       <BurgerBuilder/>
     </main>
