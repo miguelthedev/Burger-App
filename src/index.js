1ea272cec496dafcb90ba1db6c8f7f3cb133b70b
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-23 15:30:49 -0700

    Installed redux-thunk, hooked up redux dev tools, and prepared new folder structure.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
