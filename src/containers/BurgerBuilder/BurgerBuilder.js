bebfb9c19888b6e29740823e4ae4fdb1ef51386d
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-23 13:02:17 -0700

    Order now button on home page now updates properly.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchaseable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -37,7 +36,7 @@ class BurgerBuilder extends Component {
         return sum + el;
       }, 0);
 
-    this.setState({ purchaseable: sum > 0 });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -80,7 +79,7 @@ class BurgerBuilder extends Component {
           <Burger ingredients={this.props.ings}/>
           <BuildControls
             price={this.props.price}
-            purchaseable={this.state.purchaseable}
+            purchaseable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
