84f1cd3cafbbb47b3529dbe85e93e0a2e06c9caa
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-24 11:28:09 -0700

    Users can now signup.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,5 +24,19 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }
+    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBZ8CW1G_IXikmENYzUqjbB9iaWMPvvd9A', authData)
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail());
+      });
   }
 }
