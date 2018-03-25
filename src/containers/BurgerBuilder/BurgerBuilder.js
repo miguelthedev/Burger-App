32f57d96bf7efa9ed94b2d3bd07a0d98d86cff6e
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-24 18:08:44 -0700

    User is now redirected to check out page after logging in or creating an account.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,6 +35,7 @@ class BurgerBuilder extends Component {
     if(this.props.isAuthenticated) {
       this.setState({ purchasing: true });
     } else {
+      this.props.onSetRedirectPath('/checkout');
       this.props.history.push('/auth');
     }
   }
@@ -105,7 +106,8 @@ const mapDispatchToProps = dispatch => {
     onIngredientAdded: ingName => dispatch(actions.addIngredient(ingName)),
     onIngredientRemoved: ingName => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetRedirectPath: path => dispatch(actions.setAuthRedirectPath(path))
   }
 }
 
--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from '../utility';
 const initialState = {
   ingredients: null,
   totalPrice: 4,
-  error: false
+  error: false,
+  building: false
 };
 
 const INGREDIENT_PRICES = {
@@ -19,7 +20,8 @@ const addIngredient = (state, action) => {
   const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
   const updatedState = {
     ingredients: updatedIngredients,
-    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+    building: true
   }
   return updateObject(state, updatedState);
 }
@@ -29,7 +31,8 @@ const removeIngredient = (state, action) => {
   const updatedIngs = updateObject(state.ingredients, updatedIng)
   const updatedSt = {
     ingredients: updatedIngs,
-    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+    building: true
   }
   return updateObject(state, updatedSt);
 }
@@ -37,13 +40,15 @@ const removeIngredient = (state, action) => {
 const setIngredients = (state, action) => {
   return updateObject(state, {
     ingredients: {
-    salad: action.ingredients.salad,
-    bacon: action.ingredients.bacon,
-    cheese: action.ingredients.cheese,
-    meat: action.ingredients.meat,          
-  },
-  totalPrice: 4,
-  error: false});
+      salad: action.ingredients.salad,
+      bacon: action.ingredients.bacon,
+      cheese: action.ingredients.cheese,
+      meat: action.ingredients.meat,          
+    },
+    totalPrice: 4,
+    error: false,
+    building: false
+  });
 }
 
 const reducer = (state = initialState, action) => {
